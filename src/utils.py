bdbf662d005ca85d255507f5bf9522f4b9f08ff1
nasimalsajjad <33846204+nasimalsajjad@users.noreply.github.com>
2024-10-06 10:37:24 -0400

    added hyper parameter

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -16,12 +17,17 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,Y_train,X_test,Y_test,models):
+def evaluate_models(X_train,Y_train,X_test,Y_test,models,params):
     try:
         report ={}
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            greed_search = GridSearchCV(model,param,cv=3)
+            greed_search.fit(X_train,Y_train)
+            model.set_params(**greed_search.best_params_)
+            
             model.fit(X_train,Y_train)
             Y_train_pred = model.predict(X_train)
             Y_test_pred= model.predict(X_test)
